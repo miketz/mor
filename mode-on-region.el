9d5e3bb874b79c487f14bc6105911fd5f806d73b
mike <miketz@users.noreply.github.com>
2019-07-15 21:53:31 -0400

    declare closure fn mor--gen-file-name to supress flycheck warning.

--- mode-on-region.el
+++ mode-on-region.el
@@ -160,6 +160,7 @@ When using `mor-readonly-for-extra-protection-p'"
 (declare-function mor-get-tmp-buffers 'mode-on-region)
 (declare-function mor--gen-buffer-name 'mode-on-region)
 (declare-function mor--mode-on-region 'mode-on-region)
+(declare-function mor--gen-file-name 'mode-on-region)
 
 ;; TODO: Fix bug where tmp buffer won't die if the orig buffer is killed first.
 ;;       Probably need some guards when attempting to dispose markers in the
